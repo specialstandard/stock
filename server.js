216f4b1cd6508b7ae26c10c588325a007f8968cf
Brian <italissimo@gmail.com>
2016-07-28 21:41:06 +1000

    cleans post api

--- server.js
+++ server.js
@@ -5,24 +5,10 @@ var twitterInfo = require('./twitterInfo')
 var OAuth = require('oauth');
 var router = express.Router()
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-var cheerio = require('cheerio')
 var request = require('request');
-
-var UserSchema = new mongoose.Schema({
-  name: String,
-  title: String,
-  //link: String,
-  //upvotes: {type: Number, default: 0},
-  //comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-})
-var User = mongoose.model('User', UserSchema)
-mongoose.connect('mongodb://52.40.114.1/stock')
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }))
-//returns list of movies
-var movieList = []
 
 app.post('/api/twitter/stock', function(req, res, next){
   var query = req.body.query
@@ -50,24 +36,6 @@ app.post('/api/twitter/stock', function(req, res, next){
         res.send(data)
       });
 })
-
-app.post('/api/post', function(req, res, next) {
-  console.log(req.body)
-  var thisPost = new User( req.body)
-  thisPost.save( function(err, data){
-    if(err) { return next(err); }
-    res.json(data);
-  });
-});
-
-app.post('/api/delete', function(req, res, next) {
-  console.log(req.body)
-  var thisPost = new User( req.body)
-  thisPost.remove( function(err, data){
-    if(err) { return next(err); }
-    res.json(data);
-  });
-});
 
 app.use(logger('dev'));
 app.use('/', express.static(__dirname))
